b135cc0a4e18ef2da85c3643be2c67f69075c329
Paul CARTAU <pcartau@e3r2p6.42.fr>
2017-11-17 17:12:26 +0100

    temporaire

--- ft_detect_trtri.c
+++ ft_detect_trtri.c
@@ -6,29 +6,36 @@
 /*   By: pcartau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 13:24:42 by pcartau           #+#    #+#             */
-/*   Updated: 2017/11/17 08:58:29 by pcartau          ###   ########.fr       */
+/*   Updated: 2017/11/17 17:07:25 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.h"
 
-char			*ft_detect_trtri(char *src, char c)
+char		*ft_detect_trtri(char *map, char *src, char c, int j)
 {
-	char *dst;
-	int i;
-	int tmp;
-	int j;
+	char	*dst;
+	int		i;
+	int		l;
+	int		k;
 
-	i = ((int)c - 64) * 21;
-	tmp = i;
-	j = 0;
-	dst = (char *)malloc(sizeof(char) * 22);
-	while (tmp < i + 21)
+	i = (j * j) + (j + 1);
+	if (!(dst = (char *)malloc(sizeof(char) * i + 1)))
+		return (NULL);
+	l = 0;
+	k = (c - 64) * 21;
+	printf("%d", k);
+	while (l < i)
 	{
-		dst[j] = src[tmp - 21];
-		j++;
-		tmp++;
+		while (src[k] != '\n')
+		{
+			dst[l] = src[k];
+			k++;
+			l++;
+		}
+		dst[l] = map[l];
+		l++;
 	}
-	dst[j] = '\0';
+	dst[l] = '\0';
 	return (dst);
 }
