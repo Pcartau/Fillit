33b1ddd2c174ff08592f857cf42effea03fcd895
Paul CARTAU <pcartau@e3r10p8.42.fr>
2017-11-18 11:52:51 +0100

    Nouvelle version 2.0

--- ft_topshift.c
+++ ft_topshift.c
@@ -6,7 +6,7 @@
 /*   By: pcartau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 13:44:15 by pcartau           #+#    #+#             */
-/*   Updated: 2017/11/17 12:25:17 by pcartau          ###   ########.fr       */
+/*   Updated: 2017/11/18 11:51:43 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char *ft_topshift(char *map, char *str, char c, int j)
 		i = 0;
 		while (map[i])
 		{
-			if (str[i] == c && map[i - (j + 1)] == '.')
+			if (str[i] == c && str[i - (j + 1)] == '.')
 			{
 				str[i - (j + 1)] = c;
 				str[i] = '.';
@@ -31,8 +31,8 @@ char *ft_topshift(char *map, char *str, char c, int j)
 				if (map[i] == c)
 					map[i] = '.';
 			}
-			else if (str[i] == c && map[i - (j + 1)] > 127 && map[i - (j + 1)]\
-					< 0)
+			else if (str[i] == c && (map[i - (j + 1)] > 127 || \
+						map[i - (j + 1)] < 0))
 				return (NULL);
 			i++;
 		}
