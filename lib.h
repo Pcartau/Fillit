3670b236babf1019a6eac768619844c15278dc2b
Paul CARTAU <pcartau@e2r9p8.42.fr>
2017-11-21 14:06:37 +0100

    22/11/17

--- lib.h
+++ lib.h
@@ -6,7 +6,7 @@
 /*   By: pcartau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 08:52:42 by pcartau           #+#    #+#             */
-/*   Updated: 2017/11/18 14:46:46 by pcartau          ###   ########.fr       */
+/*   Updated: 2017/11/21 11:14:41 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+
+typedef	struct	s_tetri
+{
+	char letter;
+	char *map;
+	struct s_tetri *next;
+}				s_tetri;
 
 char	*ft_bottomshift(char *map, char *str, char c, int j);
 char	*ft_leftshift(char *map, char *str, char c, int j);
@@ -29,5 +36,6 @@ char	*ft_strdup(const char *src);
 int		ft_checker(char *map, char c, int i);
 int		ft_checkpoint(char *map);
 size_t	ft_strlen(const char *src);
-
+void	ft_classer_tetri(char *str, char c, int j);
+char	*ft_return_tetri(s_tetri *tetri, char c);
 #endif
