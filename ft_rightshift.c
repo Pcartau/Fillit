a55702ec04c2029fdc07e72958b93f459ae6548c
Paul CARTAU <pcartau@e2r7p19.42.fr>
2017-11-24 17:03:44 +0100

    Modifie

--- ft_rightshift.c
+++ ft_rightshift.c
@@ -6,22 +6,38 @@
 /*   By: pcartau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 13:43:23 by pcartau           #+#    #+#             */
-/*   Updated: 2017/11/18 11:22:13 by pcartau          ###   ########.fr       */
+/*   Updated: 2017/11/24 16:36:32 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.h"
 
-char *ft_rightshift(char *map, char *str, char c)
+static int	condition_right(char *tetri, char c, int xy)
+{
+	int i;
+
+	i = 1;
+	while (i < xy)
+	{
+		if (tetri[xy * i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char *ft_rightshift(char *map, char *str, char c, int xy)
 {
 	int i;
 	int l;
 
+	if (!(condition_right(str, c, xy)))
+		return (str);
 	l = ft_strlen(map);
 	i = l - 1;
 	while (map[i])
 	{
-		if (str[i] == c && map[i + 1] == '.')
+		if (str[i] == c && map[i] == '.')
 		{
 			str[i + 1] = c;
 			str[i] = '.';
